7c967be67699e8d6cd04572c7f43f2069d915e44
Chandra Shekhar Das <ch.shekhardas@gmail.com>
2024-01-11 19:12:40 +0600

    added code for json.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import docx
 from category import getCategories
+import json
 
 url = 'https://b.hatena.ne.jp'
 allLinksByCategoryDict={}
@@ -10,19 +11,40 @@ categories = getCategories(baseUrl=url)
 
 
 class LinkWithTitle:
-    def __init__(self,title,link):
+    def __init__(self,title,link,description,subjectList):
         self.title = title
         self.link = link
+        self.description = description
+        self.subjectList = subjectList
 
+    def toJson(obj):
+        return json.dumps(obj, default=lambda obj: obj.__dict__,ensure_ascii=False)
 
 #get all links.
 def getLinksWithTitle(content):
-    linkWithTitle=[]
+    linkWithTitle={}
+
     soup = BeautifulSoup(content, 'xml')
     allFetchedElements = soup.find_all('item')
+    countItem=1
     for element in allFetchedElements:
-        obj = LinkWithTitle(title=element.find_all('title')[0].contents[0],link=element.find_all('link')[0].contents[0])
-        linkWithTitle.append(obj)
+        _subjectList = {}
+        _title = element.find_all('title')[0].contents[0]
+        _link = element.find_all('link')[0].contents[0]
+        _description = ''
+        try:
+            _description = element.find_all('description')[0].contents[0]
+        except:
+            _description=''
+        _subjectWithTags = element.find_all('dc:subject')
+        countSubject=1
+        for subjectElement in _subjectWithTags:
+            _subjectList[countSubject] = subjectElement.contents[0]
+            countSubject = countSubject+1
+
+        obj = LinkWithTitle(title=_title,link=_link, description=_description,subjectList = _subjectList)
+        linkWithTitle[countItem]=obj.toJson()
+        countItem = countItem+1
     return linkWithTitle
 
 
@@ -56,9 +78,13 @@ def getLinks_Title():
 
 fillDictionaryWithData()
 
-getOnlyLinks()
+#getOnlyLinks()
 
-getLinks_Title()
+#getLinks_Title()
+
+#saving data into a json file.
+jsonFile = open('C:\\Users\\shekhar\\Downloads\\object_list.json','w',encoding='utf-16')
+json.dump(allLinksByCategoryDict,jsonFile,ensure_ascii=False)
 
 
 
